06fb7c62f2fc87de0f0ad118a856852902be5026
Pjineon <flowervelt@naver.com>
2026-01-01 23:16:18 +0900

    chore: 프로젝트 초기 세팅

---
+++ android/app/src/main/kotlin/com/example/snsp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.snsp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/home.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BBANTO', style:TextStyle(color: Colors.white, letterSpacing:2.0, fontSize:30.0, fontWeight: FontWeight.bold)),
+        centerTitle: true,
+        backgroundColor: Colors.amber[700],
+        elevation: 0.0,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Hello'),
+            Divider(
+              height: 100.0,
+              thickness: 5,
+            )
+          ],
+        ),
+
+      ),
+    );
+  }
+}
